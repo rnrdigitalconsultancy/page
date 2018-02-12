72cb1ebb4cc9be041e6952b5cfaf539121134bb4
rnrdigitalconsultancy <info@rnrdigitalconsultancy.com>
2018-02-12 15:24:19 +0800

    rnr update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -58,8 +58,16 @@ $(document).on("ready",function(){
         density:13000,
         parallax:false,
         particleRadius: 4,
-        lineWidth: .75
+        lineWidth: .75,
+        parallaxMultiplier: 7
     });
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+    }
+  );
 
     $('.carousel').carousel({fullWidth: true});
 
